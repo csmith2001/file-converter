c95f379ac0737fab65a455bd743884e1ce480d83
csmith2001 <cmansmith01@gmail.com>
2026-06-20 14:13:00 -0400

    feature to prompt user for specified file types (multiple selection). Feature to list unique file suffixes chosen - will be used later to list files in directory (maybe? might not be useful)

--- main.py
+++ main.py
@@ -1,5 +1,18 @@
 from pathlib import Path
 from PIL import Image
-import pillow_heif
+from tkinter import filedialog
+import tkinter as tk, pillow_heif
 
 pillow_heif.register_heif_opener()
+
+root = tk.Tk()
+root.withdraw()
+
+img_types = "*.jpg *.jpeg *.png *.heic *.webp"
+file_types = [("Image Files:", img_types)]
+
+files = filedialog.askopenfilenames(filetypes=file_types)
+
+suffixes = list({f"*{Path(file).suffix.lower()}" for file in files})
+
+print(suffixes)
